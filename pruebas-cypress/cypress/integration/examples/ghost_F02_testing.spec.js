3b75c374b9c2ae7eea5673bad4ca8a19bad48a51
Marisela Del Valle <mac@Mac-mini-de-Mac.local>
2021-03-06 20:37:34 -0500

    use config vars in F02

--- pruebas-cypress/cypress/integration/examples/ghost_F02_testing.spec.js
+++ pruebas-cypress/cypress/integration/examples/ghost_F02_testing.spec.js
@@ -1,18 +1,21 @@
 describe('Ghost', function () {
+    const ghost_url = Cypress.env('GHOST_URL')
+    const email = Cypress.env('EMAIL')
+    const password = Cypress.env('PASSWORD')
 
     beforeEach(()=>{
         cy.viewport(1280,800)
-        cy.visit('http://localhost:2368/ghost/#/signin')
-            cy.get('#ember8').type('j.guataquira@uniandes.edu.co')
-            cy.get('#ember10').type('Uniandespili77')
-            cy.get('#ember12 > span').click()
-            cy.wait(1000)
+        cy.visit(ghost_url + '/ghost/#/signin')
+        cy.get('#ember8').type(email)
+        cy.get('#ember10').type(password)
+        cy.get('#ember12 > span').click()
+        cy.wait(1000)
     })
 
     it('Cierre de sesión exitoso es Ghost (Esenario 1 para F02)', function() {
         cy.get('.w3.mr1.fill-darkgrey').click()
         cy.get('.dropdown-menu.dropdown-triangle-top').contains('Sign Out').click()
-        cy.url().should('eq', 'http://localhost:2368/__/#/signin')
+        cy.url().should('eq', ghost_url + '/__/#/signin')
     })
 
     it('Cierre de sesión exitoso es Ghost (Esenario 2 para F02)', function() {
--- pruebas-cypress/cypress/integration/examples/ghost_F06_testing.spec.js
+++ pruebas-cypress/cypress/integration/examples/ghost_F06_testing.spec.js
@@ -43,33 +43,33 @@ describe('Ghost', function () {
         cy.screenshot()
     })
 
-    // it('Edición de correo con formato válido (Escenario 4 para F06)', function() {
-    //     const ghost_url = Cypress.env('GHOST_URL');
+    it('Edición de correo con formato válido (Escenario 4 para F06)', function() {
+        const ghost_url = Cypress.env('GHOST_URL');
 
-    //     cy.get('.w3.mr1.fill-darkgrey').click()
-    //     cy.get('.dropdown-menu.dropdown-triangle-top').contains('Your Profile').click()
-    //     cy.get('#user-email').click({force: true}).clear().type('correovalido2@correo.com')
-    //     cy.get('.gh-btn-blue').click()
-    //     cy.get('.gh-btn-red > span').first().contains('Retry').should('not.exist')
-    //     cy.wait(1000)
+        cy.get('.w3.mr1.fill-darkgrey').click()
+        cy.get('.dropdown-menu.dropdown-triangle-top').contains('Your Profile').click()
+        cy.get('#user-email').click({force: true}).clear().type('correovalido2@correo.com')
+        cy.get('.gh-btn-blue').click()
+        cy.get('.gh-btn-red > span').first().contains('Retry').should('not.exist')
+        cy.wait(1000)
         
 
-    //     cy.get('.w3.mr1.fill-darkgrey').click()
-    //     cy.get('.user-menu-signout').click()
-    //     cy.wait(1000)
+        cy.get('.w3.mr1.fill-darkgrey').click()
+        cy.get('.user-menu-signout').click()
+        cy.wait(1000)
 
-    //     cy.visit(ghost_url + '/ghost/#/signin')
-    //     cy.get('.email').type('correovalido2@correo.com')
-    //     cy.get('.password').type(password)
-    //     cy.get('.login').click()
-    //     cy.wait(1000)
+        cy.visit(ghost_url + '/ghost/#/signin')
+        cy.get('.email').type('correovalido2@correo.com')
+        cy.get('.password').type(password)
+        cy.get('.login').click()
+        cy.wait(1000)
 
-    //     cy.get('.w3.mr1.fill-darkgrey').click()
-    //     cy.get('.dropdown-menu.dropdown-triangle-top').contains('Your Profile').click()
-    //     cy.get('#user-email').click({force: true}).clear().type(email)
-    //     cy.get('.gh-btn-blue').click()
-    //     cy.wait(1000)
-    //     cy.screenshot()
-    // })
+        cy.get('.w3.mr1.fill-darkgrey').click()
+        cy.get('.dropdown-menu.dropdown-triangle-top').contains('Your Profile').click()
+        cy.get('#user-email').click({force: true}).clear().type(email)
+        cy.get('.gh-btn-blue').click()
+        cy.wait(1000)
+        cy.screenshot()
+    })
 
 })
