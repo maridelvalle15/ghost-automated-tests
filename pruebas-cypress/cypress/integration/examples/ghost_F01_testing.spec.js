2d13611916f15d0521e20b1e15f658c0886c0196
Marisela Del Valle <mac@Mac-mini-de-Mac.local>
2021-03-06 20:24:49 -0500

    use config vars in F01, F06 and F07

--- pruebas-cypress/cypress/integration/examples/ghost_F01_testing.spec.js
+++ pruebas-cypress/cypress/integration/examples/ghost_F01_testing.spec.js
@@ -1,19 +1,22 @@
 describe('Ghost under monkeys', function() {
+    const ghost_url = Cypress.env('GHOST_URL')
+    const email = Cypress.env('EMAIL')
+    const password = Cypress.env('PASSWORD')
 
     it('Login fallido en Ghost (Esenario 1 para F01)', function() {
-        cy.visit('http://localhost:2368/ghost/#/signin');
+        cy.visit(ghost_url + '/ghost/#/signin');
         cy.wait(1000)
-        cy.get('#ember8').type('jguataquira@uniandes.edu.co')
-        cy.get('#ember10').type('Uniandespili77')
+        cy.get('#ember8').type(email)
+        cy.get('#ember10').type(password)
         cy.get('#ember12 > span').click()
         cy.wait(1000)
     })
 
     it('Login exitoso es Ghost (Esenario 2 para F01)', function() {
-        cy.visit('http://localhost:2368/ghost/#/signin');
+        cy.visit(ghost_url + '/ghost/#/signin');
         cy.wait(1000);
-        cy.get('#ember8').type('j.guataquira@uniandes.edu.co')
-        cy.get('#ember10').type('Uniandespili77')
+        cy.get('#ember8').type(email)
+        cy.get('#ember10').type(password)
         cy.get('#ember12 > span').click()
         cy.wait(10000)
     })
--- pruebas-cypress/cypress/integration/examples/ghost_F06_testing.spec.js
+++ pruebas-cypress/cypress/integration/examples/ghost_F06_testing.spec.js
@@ -1,12 +1,15 @@
 describe('Ghost', function () {
+    const ghost_url = Cypress.env('GHOST_URL')
+    const email = Cypress.env('EMAIL')
+    const password = Cypress.env('PASSWORD')
 
     beforeEach(()=>{
         cy.viewport(1280,800)
-        cy.visit('http://localhost:2372/ghost/#/signin')
-            cy.get('.email').type('marisela.delvalle93@gmail.com')
-            cy.get('.password').type('12345678910')
-            cy.get('.login').click()
-            cy.wait(1000)
+        cy.visit(ghost_url + '/ghost/#/signin')
+        cy.get('.email').type(email)
+        cy.get('.password').type(password)
+        cy.get('.login').click()
+        cy.wait(1000)
     })
 
     it('Edición de correo con formato inválido (Escenario 1 para F06)', function() {
@@ -15,6 +18,7 @@ describe('Ghost', function () {
         cy.get('#user-email').click({force: true}).clear().type('correo inválido')
         cy.get('.gh-btn-blue').click()
         cy.get('.gh-btn-red > span').first().contains('Retry')
+        cy.screenshot()
     })
 
     it('Edición de correo con formato inválido (Escenario 2 para F06)', function() {
@@ -23,6 +27,7 @@ describe('Ghost', function () {
         cy.get('#user-email').click({force: true}).clear().type('correoinvalido@correo')
         cy.get('.gh-btn-blue').click()
         cy.get('.gh-btn-red > span').first().contains('Retry')
+        cy.screenshot()
     })
 
     it('Edición de correo con formato válido (Escenario 3 para F06)', function() {
@@ -32,35 +37,39 @@ describe('Ghost', function () {
         cy.get('.gh-btn-blue').click()
         cy.get('.gh-btn-red > span').first().contains('Retry').should('not.exist')
         cy.wait(1000)
-        cy.get('#user-email').click({force: true}).clear().type('marisela.delvalle93@gmail.com')
+        cy.get('#user-email').click({force: true}).clear().type(email)
         cy.get('.gh-btn-blue').click()
         cy.wait(1000)
+        cy.screenshot()
     })
 
-    it('Edición de correo con formato válido (Escenario 4 para F06)', function() {
-        cy.get('.w3.mr1.fill-darkgrey').click()
-        cy.get('.dropdown-menu.dropdown-triangle-top').contains('Your Profile').click()
-        cy.get('#user-email').click({force: true}).clear().type('correovalido2@correo.com')
-        cy.get('.gh-btn-blue').click()
-        cy.get('.gh-btn-red > span').first().contains('Retry').should('not.exist')
-        cy.wait(1000)
+    // it('Edición de correo con formato válido (Escenario 4 para F06)', function() {
+    //     const ghost_url = Cypress.env('GHOST_URL');
+
+    //     cy.get('.w3.mr1.fill-darkgrey').click()
+    //     cy.get('.dropdown-menu.dropdown-triangle-top').contains('Your Profile').click()
+    //     cy.get('#user-email').click({force: true}).clear().type('correovalido2@correo.com')
+    //     cy.get('.gh-btn-blue').click()
+    //     cy.get('.gh-btn-red > span').first().contains('Retry').should('not.exist')
+    //     cy.wait(1000)
         
 
-        cy.get('.w3.mr1.fill-darkgrey').click()
-        cy.get('.user-menu-signout').click()
-        cy.wait(1000)
+    //     cy.get('.w3.mr1.fill-darkgrey').click()
+    //     cy.get('.user-menu-signout').click()
+    //     cy.wait(1000)
 
-        cy.visit('http://localhost:2372/ghost/#/signin')
-        cy.get('.email').type('correovalido2@correo.com')
-        cy.get('.password').type('12345678910')
-        cy.get('.login').click()
-        cy.wait(1000)
+    //     cy.visit(ghost_url + '/ghost/#/signin')
+    //     cy.get('.email').type('correovalido2@correo.com')
+    //     cy.get('.password').type(password)
+    //     cy.get('.login').click()
+    //     cy.wait(1000)
 
-        cy.get('.w3.mr1.fill-darkgrey').click()
-        cy.get('.dropdown-menu.dropdown-triangle-top').contains('Your Profile').click()
-        cy.get('#user-email').click({force: true}).clear().type('marisela.delvalle93@gmail.com')
-        cy.get('.gh-btn-blue').click()
-        cy.wait(1000)
-    })
+    //     cy.get('.w3.mr1.fill-darkgrey').click()
+    //     cy.get('.dropdown-menu.dropdown-triangle-top').contains('Your Profile').click()
+    //     cy.get('#user-email').click({force: true}).clear().type(email)
+    //     cy.get('.gh-btn-blue').click()
+    //     cy.wait(1000)
+    //     cy.screenshot()
+    // })
 
 })
