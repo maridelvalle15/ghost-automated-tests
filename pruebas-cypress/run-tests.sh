7a74a0eaa949710318d9e528e981d5ff385319f5
Marisela Del Valle <mac@Mac-mini-de-Mac.local>
2021-03-06 20:38:58 -0500

    bash script to execute tests

---
+++ pruebas-cypress/run-tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+node_modules/cypress/bin/cypress run --env GHOST_URL=http://localhost:2370,EMAIL=marisela.delvalle93@gmail.com,PASSWORD=12345678910
+node_modules/cypress/bin/cypress run --env GHOST_URL=http://localhost:2372,EMAIL=marisela.delvalle93@gmail.com,PASSWORD=12345678910
