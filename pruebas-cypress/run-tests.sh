07774fc4d45d5f6c4fe123bdf56ae24e127f7145
Marisela Del Valle <mac@Mac-mini-de-Mac.local>
2021-03-08 00:42:23 -0500

    save screenshots and generate report from script

--- pruebas-cypress/run-tests.sh
+++ pruebas-cypress/run-tests.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
-node_modules/cypress/bin/cypress run --env GHOST_URL=http://localhost:2368,EMAIL=j.guataquira@uniandes.edu.co,PASSWORD=Uniandespili77,GHOST_VERSION=reference
-node_modules/cypress/bin/cypress run --env GHOST_URL=http://localhost:2369,EMAIL=j.guataquira@uniandes.edu.co,PASSWORD=Uniandespili77,GHOST_VERSION=test
+mkdir vrt_screenshots
+mkdir vrt_screenshots/examples
+node_modules/cypress/bin/cypress run --env GHOST_URL=http://localhost:2372,EMAIL=marisela.delvalle93@gmail.com,PASSWORD=12345678910,GHOST_VERSION=reference,VRT=true --spec cypress/integration/examples/ghost_F01_testing.spec.js
+mkdir vrt_screenshots/examples/ghost_F01_testing.spec.js
+mkdir vrt_screenshots/examples/ghost_F01_testing.spec.js/bitmaps_reference
+cp cypress/screenshots/examples/ghost_F01_testing.spec.js/bitmaps_reference/* vrt_screenshots/examples/ghost_F01_testing.spec.js/bitmaps_reference/
+node_modules/cypress/bin/cypress run --env GHOST_URL=http://localhost:2370,EMAIL=marisela.delvalle93@gmail.com,PASSWORD=12345678910,GHOST_VERSION=test,VRT=true --spec cypress/integration/examples/ghost_F01_testing.spec.js
+mkdir vrt_screenshots/examples/ghost_F01_testing.spec.js/bitmaps_test
+cp cypress/screenshots/examples/ghost_F01_testing.spec.js/bitmaps_test/* vrt_screenshots/examples/ghost_F01_testing.spec.js/bitmaps_test/
+node_modules/cypress/bin/cypress run --env GHOST_URL=http://localhost:2372,EMAIL=marisela.delvalle93@gmail.com,PASSWORD=12345678910,GHOST_VERSION=reference,VRT=true --spec cypress/integration/examples/ghost_F03_testing.spec.js
+mkdir vrt_screenshots/examples/ghost_F03_testing.spec.js
+mkdir vrt_screenshots/examples/ghost_F03_testing.spec.js/bitmaps_reference
+cp cypress/screenshots/examples/ghost_F03_testing.spec.js/bitmaps_reference/* vrt_screenshots/examples/ghost_F03_testing.spec.js/bitmaps_reference/
+node_modules/cypress/bin/cypress run --env GHOST_URL=http://localhost:2370,EMAIL=marisela.delvalle93@gmail.com,PASSWORD=12345678910,GHOST_VERSION=test,VRT=true --spec cypress/integration/examples/ghost_F03_testing.spec.js
+mkdir vrt_screenshots/examples/ghost_F03_testing.spec.js/bitmaps_test
+cp cypress/screenshots/examples/ghost_F03_testing.spec.js/bitmaps_test/* vrt_screenshots/examples/ghost_F03_testing.spec.js/bitmaps_test/
+
+cp -R vrt_screenshots vrt_report/
+
+cd vrt_report/
+npm install
+cd pathfinder
+node index.js
+cd ..
+npm run start
