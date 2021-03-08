680ded89ba44cbae20dd3896d476656d6f4ab555
Marisela Del Valle <mac@Mac-mini-de-Mac.local>
2021-03-07 22:58:48 -0500

    fix F05

--- pruebas-cypress/cypress/integration/examples/ghost_F05_testing.spec.js
+++ pruebas-cypress/cypress/integration/examples/ghost_F05_testing.spec.js
@@ -2,19 +2,19 @@ describe('Ghost', function () {
     const ghost_url = Cypress.env('GHOST_URL')
     const email = Cypress.env('EMAIL')
     const password = Cypress.env('PASSWORD')
-    const ghost_version = 'reference'
+    const ghost_version = Cypress.env('GHOST_VERSION')
 
     beforeEach(()=>{
         cy.viewport(1280,800)
         cy.visit(ghost_url + '/ghost/#/signin')
             cy.get('#ember8').type(email)
-            cy.get('#ember10').type(pasword)
+            cy.get('#ember10').type(password)
             cy.get('#ember12 > span').click()
             cy.wait(1000)
     })
 
     it('Eliminación de Posts - escenario 1',function(){
-        cy.get('#ember28').click()
+        cy.get('a[href*="#/posts/"]').first().click()
         if (ghost_version == 'reference'){
             cy.screenshot('/bitmaps_reference/F05_e1_p1')
         }
@@ -53,8 +53,8 @@ describe('Ghost', function () {
     })
 
     it('Eliminación de Posts - escenario 2',function(){
-        cy.get('#ember28').click()
-        cy.get('.gh-content-status-draft.gh-badge.gh-badge-purple.nowrap').contains('Draft').click({force: true})
+        cy.get('a[href*="#/posts/"]').first().click()
+        cy.get('.gh-content-status-draft').first().click({force: true})
         cy.get('.post-settings').click()
         cy.get('.gh-btn.gh-btn-hover-red.gh-btn-icon.settings-menu-delete-button').click()
         cy.get('.gh-btn.gh-btn-red.gh-btn-icon.ember-view').contains('Delete').click()
