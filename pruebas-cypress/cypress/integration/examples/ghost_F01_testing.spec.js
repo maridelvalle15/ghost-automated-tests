1de93b951def0d3fc61270ca70fb4a7489fae7e1
Marisela Del Valle <mac@Mac-mini-de-Mac.local>
2021-03-13 15:11:44 -0500

    added random data to F01 and F03

--- pruebas-cypress/cypress/integration/examples/ghost_F01_testing.spec.js
+++ pruebas-cypress/cypress/integration/examples/ghost_F01_testing.spec.js
@@ -1,3 +1,5 @@
+const faker = require('faker');
+
 describe('Ghost under monkeys', function() {
     const ghost_url = Cypress.env('GHOST_URL')
     const email = Cypress.env('EMAIL')
@@ -5,44 +7,84 @@ describe('Ghost under monkeys', function() {
     const ghost_version = Cypress.env('GHOST_VERSION')
     const is_vrt = Cypress.env('VRT')
 
-    it('Login fallido en Ghost (Esenario 1 para F01)', function() {
-        cy.visit(ghost_url + '/ghost/#/signin');
+    for (i = 1; i <= 5; i++) {
+        // 5 escenario aleatorio
+        it(`Login fallido en Ghost para correo registrado - escenario 1 - ${i}`, function() {
+            cy.visit(ghost_url + '/ghost/#/signin');
+            
+            if (is_vrt && ghost_version == 'reference'){
+                cy.screenshot('/bitmaps_reference/F01_e1_p1')
+            }
+            else if (is_vrt && ghost_version == 'test') {
+                cy.screenshot('/bitmaps_test/F01_e1_p1')
+            }
+            cy.wait(1000)
+            cy.get('#ember8').type(email)
+            
+            if (is_vrt && ghost_version == 'reference'){
+                cy.screenshot('/bitmaps_reference/F01_e1_p2')
+            }
+            else if (is_vrt && ghost_version == 'test') {
+                cy.screenshot('/bitmaps_test/F01_e1_p2')
+            }
+            let randomPassword = faker.internet.password();
+            cy.get('#ember10').type(randomPassword)
+            
+            if (is_vrt && ghost_version == 'reference'){
+                cy.screenshot('/bitmaps_reference/F01_e1_p3')
+            }
+            else if (is_vrt && ghost_version == 'test') {
+                cy.screenshot('/bitmaps_test/F01_e1_p3')
+            }
+            cy.get('#ember12 > span').click()
+            cy.wait(1000)
+            
+            if (is_vrt && ghost_version == 'reference'){
+                cy.screenshot('/bitmaps_reference/F01_e1_p4')
+            }
+            else if (is_vrt && ghost_version == 'test') {
+                cy.screenshot('/bitmaps_test/F01_e1_p4')
+            }
+        })
+    }
+
+    for (i = 1; i <= 5; i++) {
+        // 5 escenario aleatorio
+        it(`Login fallido en Ghost para contraseña registrada - escenario 2 - ${i}`, function() {
+            cy.visit(ghost_url + '/ghost/#/signin');
+            
+            cy.wait(1000)
+
+            let randomEmail = faker.internet.email();
+            cy.get('#ember8').type(randomEmail)
+            cy.get('#ember10').type(password)
+            cy.get('#ember12 > span').click()
         
-        if (is_vrt && ghost_version == 'reference'){
-            cy.screenshot('/bitmaps_reference/F01_e1_p1')
-        }
-        else if (is_vrt && ghost_version == 'test') {
-            cy.screenshot('/bitmaps_test/F01_e1_p1')
-        }
-        cy.wait(1000)
-        cy.get('#ember8').type(email)
+        })
+    }
+
+    var i, 
+    dataPool = [];
+    for (i = 1; i <= 5; i++) {
+        dataPool.push( {
+            'email' : faker.internet.email(),
+            'password' : faker.internet.password()  
+        });
+    };
+    dataPool.forEach(function(data, i) {
+        // 5 dataPool
+        it(`Login fallido en Ghost para contraseña registrada - escenario 3 - ${i+1}`, function() {
+            cy.visit(ghost_url + '/ghost/#/signin');
+            
+            cy.wait(1000)
+            cy.get('#ember8').type(data.email)
+            cy.get('#ember10').type(data.password)
+            cy.get('#ember12 > span').click()
         
-        if (is_vrt && ghost_version == 'reference'){
-            cy.screenshot('/bitmaps_reference/F01_e1_p2')
-        }
-        else if (is_vrt && ghost_version == 'test') {
-            cy.screenshot('/bitmaps_test/F01_e1_p2')
-        }
-        cy.get('#ember10').type('1919191')
-        
-        if (is_vrt && ghost_version == 'reference'){
-            cy.screenshot('/bitmaps_reference/F01_e1_p3')
-        }
-        else if (is_vrt && ghost_version == 'test') {
-            cy.screenshot('/bitmaps_test/F01_e1_p3')
-        }
-        cy.get('#ember12 > span').click()
-        cy.wait(1000)
-        
-        if (is_vrt && ghost_version == 'reference'){
-            cy.screenshot('/bitmaps_reference/F01_e1_p4')
-        }
-        else if (is_vrt && ghost_version == 'test') {
-            cy.screenshot('/bitmaps_test/F01_e1_p4')
-        }
+        })
     })
 
-    it('Login exitoso es Ghost (Esenario 2 para F01)', function() {
+    it('Login exitoso es Ghost (Esenario 4 para F01)', function() {
         cy.visit(ghost_url + '/ghost/#/signin');
         cy.wait(1000);
         cy.get('#ember8').type(email)
--- pruebas-cypress/cypress/integration/examples/ghost_F03_testing.spec.js
+++ pruebas-cypress/cypress/integration/examples/ghost_F03_testing.spec.js
@@ -1,3 +1,5 @@
+const faker = require('faker');
+
 describe('Ghost', function () {
     const ghost_url = Cypress.env('GHOST_URL')
     const email = Cypress.env('EMAIL')
@@ -14,44 +16,57 @@ describe('Ghost', function () {
         cy.wait(1000)
     })
 
-    it('Ajustes en el diseño del blog - escenario 1',function(){
-        cy.get('a[href*="#/settings/design/"]').click()
-        cy.wait(1000)
-        if (is_vrt && ghost_version == 'reference'){
-            cy.screenshot('/bitmaps_reference/F03_e1_p1')
-        }
-        else if (is_vrt && ghost_version == 'test') {
-            cy.screenshot('/bitmaps_test/F03_e1_p1')
-        }
-        cy.wait(1000)
-        cy.get('.gh-blognav-container > .gh-blognav > .gh-blognav-item > .gh-blognav-line > .gh-blognav-label > .gh-input').first().type('Nueva Pestaña',{force: true})
-        cy.wait(1000)
-        if (is_vrt && ghost_version == 'reference'){
-            cy.screenshot('/bitmaps_reference/F03_e1_p2')
-        }
-        else if (is_vrt && ghost_version == 'test') {
-            cy.screenshot('/bitmaps_test/F03_e1_p2')
-        }
-        cy.wait(1000)
-        cy.get('.gh-blognav-container > .gh-blognav > .gh-blognav-item > .gh-blognav-add').first().click({force: true})
-        cy.wait(1000)
-        if (is_vrt && ghost_version == 'reference'){
-            cy.screenshot('/bitmaps_reference/F03_e1_p3')
-        }
-        else if (is_vrt && ghost_version == 'test') {
-            cy.screenshot('/bitmaps_test/F03_e1_p3')
-        }
-        cy.wait(1000)
-        cy.get('.gh-btn.gh-btn-blue.gh-btn-icon.ember-view').click()
-        cy.get('.gh-btn').first().contains('Saved')
-        if (is_vrt && ghost_version == 'reference'){
-            cy.screenshot('/bitmaps_reference/F03_e1_p4')
-        }
-        else if (is_vrt && ghost_version == 'test') {
-            cy.screenshot('/bitmaps_test/F03_e1_p4')
-        }
+    var i, 
+    dataPool = [];
+    for (i = 1; i <= 5; i++) {
+        dataPool.push( {
+            'label' : faker.lorem.words(),
+            'url' : faker.lorem.word()  
+        });
+    };
+    dataPool.forEach(function(data, i) {
+        // 5 dataPool
+        it(`Ajustes en el diseño del blog - escenario 1 - ${i+1}`,function(){
+            cy.get('a[href*="#/settings/design/"]').click()
+            cy.wait(1000)
+            if (is_vrt && ghost_version == 'reference'){
+                cy.screenshot('/bitmaps_reference/F03_e1_p1')
+            }
+            else if (is_vrt && ghost_version == 'test') {
+                cy.screenshot('/bitmaps_test/F03_e1_p1')
+            }
+            cy.wait(1000)
+            cy.get('.gh-blognav-container > .gh-blognav > .gh-blognav-item > .gh-blognav-line > .gh-blognav-label > .gh-input').first().type(data.label,{force: true})
+            cy.get('.gh-blognav-container > .gh-blognav > .gh-blognav-item > .gh-blognav-line > .gh-blognav-url > .gh-input').first().type(data.url,{force: true})
+            cy.wait(1000)
+            if (is_vrt && ghost_version == 'reference'){
+                cy.screenshot('/bitmaps_reference/F03_e1_p2')
+            }
+            else if (is_vrt && ghost_version == 'test') {
+                cy.screenshot('/bitmaps_test/F03_e1_p2')
+            }
+            cy.wait(1000)
+            cy.get('.gh-blognav-container > .gh-blognav > .gh-blognav-item > .gh-blognav-add').first().click({force: true})
+            cy.wait(1000)
+            if (is_vrt && ghost_version == 'reference'){
+                cy.screenshot('/bitmaps_reference/F03_e1_p3')
+            }
+            else if (is_vrt && ghost_version == 'test') {
+                cy.screenshot('/bitmaps_test/F03_e1_p3')
+            }
+            cy.wait(1000)
+            cy.get('.gh-btn.gh-btn-blue.gh-btn-icon.ember-view').click()
+            cy.get('.gh-btn').first().contains('Saved')
+            if (is_vrt && ghost_version == 'reference'){
+                cy.screenshot('/bitmaps_reference/F03_e1_p4')
+            }
+            else if (is_vrt && ghost_version == 'test') {
+                cy.screenshot('/bitmaps_test/F03_e1_p4')
+            }
+    
+        })
 
-    })
+    });
 
     it('Ajustes en el diseño del blog - escenario 2',function(){
         cy.get('a[href*="#/settings/design/"]').click()
